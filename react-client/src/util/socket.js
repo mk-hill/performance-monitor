1f4a67d33b08d045654beeed311c26767e4b86d3
mk-hill <40850298+mk-hill@users.noreply.github.com>
2019-02-12 22:19:07 -0600

    Delete unused files

--- react-client/src/util/socket.js
+++
@@ -1,9 +0,0 @@
-import socketio from 'socket.io-client';
-
-const socket = socketio.connect(
-  process.env.REACT_APP_SERVER_URL || 'http://localhost:8181'
-);
-
-socket.emit('clientAuth', process.env.REACT_APP_DASHBOARD_KEY || 'dashboardKey');
-
-export default socket;
